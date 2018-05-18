3fbf7409041c7cb9786c836f9ae954e03cd2d81f
Ariel Zhen <zhenpingping@live.com>
2018-05-18 23:30:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,7 +33,7 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true
-  elsif !won?(board) && !full?(board) || !won?(board)
+  elsif !won?(board) && !full?(board) 
     return false
   end
 end
